a4352411de515dc585173d027de9ea73a1c64484
leonardo <leochinelato9852@gmail.com>
2024-03-12 06:26:47 -0300

    Adicionar .get e corrigir o while

--- higher_lower/app.py
+++ higher_lower/app.py
@@ -3,14 +3,15 @@ from data import data
 import random
 import os
 
+
 def random_account():
     return random.choice(data)
 
 
 def format_data(account):
-    name = account['name']
-    description = account['description']
-    country = account['country']
+    name = account.get('name')
+    description = account.get('description')
+    country = account.get('country')
     return f'{name}, a {description}, from {country}'
 
 
@@ -31,23 +32,24 @@ def game():
     while game_isrunning:
         account_a = account_b
         account_b = random_account()
-        print(f'a: {account_a},\n b: {account_b}')
         while account_a == account_b:
             account_b = random_account()
-            print(f'Compare A: {format_data(account_a)}.')
-            print(vs)
-            print(f'Against B: {format_data(account_b)}')
-            answer = input('Who has more followers? Type "A" or "B": ').lower()
-            iscorrect = check(answer, account_a['follower_count'], account_b['follower_count'])
+        print(f'Compare A: {format_data(account_a)}.')
+        print(vs)
+        print(f'Against B: {format_data(account_b)}')
+        answer = input('Who has more followers? Type "A" or "B": ').lower()
+        iscorrect = check(
+            answer, account_a.get('follower_count'), account_b.get('follower_count')
+        )
 
-            os.system('cls')
-            print(logo)
-            if iscorrect:
-                score += 1
-                print(f'Yes. You are right! Current score: {score}')
-            else:
-                print(f'Sorry, that is wrong. Final score: {score}')
-                game_isrunning = False
+        os.system('cls')
+        print(logo)
+        if iscorrect:
+            score += 1
+            print(f'Yes. You are right! Current score: {score}')
+        else:
+            game_isrunning = False
+            print(f'Sorry, that is wrong. Final score: {score}')
 
 
 if __name__ == '__main__':
