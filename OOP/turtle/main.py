f7479972dc6cb23575a085b172060e18cb94688d
leonardo <leochinelato9852@gmail.com>
2024-04-10 13:29:43 -0300

    add

---
+++ OOP/turtle/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+cleiton = Turtle()
+
+print(cleiton)
+cleiton.shape("turtle")
+
+screen = Screen()
+
+cleiton.forward(100)
+cleiton.backward(100)
+
+screen.exitonclick()
